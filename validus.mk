3817817aa3b095616273d60c67904df471e0faec
Adam <ovadia.adam@gmail.com>
2015-02-26 10:13:19 -0500

    Update and rename cm.mk to validus.mk

--- validus.mk
+++ validus.mk
@@ -2,18 +2,18 @@
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Validus stuff.
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/validus/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/e980/e980.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := e980
-PRODUCT_NAME := cm_e980
+PRODUCT_NAME := validus_e980
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-E980
 PRODUCT_MANUFACTURER := LGE
